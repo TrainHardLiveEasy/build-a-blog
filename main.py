558f93d4c453185982c6dbfaf021673e20853836
TrainHardLiveEasy <kolleradam2005@gmail.com>
2018-04-15 22:51:54 -0500

    made blog class

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, redirect, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:Ih8nickcage@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    body = db.Column(db.String(300))
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+
+
+
+
+
